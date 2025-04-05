ce599f47f9c246623339d1cc2cdbeab0b81f34ce
Fernando085-cybernetico <fernandonogueira26@icloud.com>
2025-04-05 15:46:51 -0300

    Academia

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,24 @@
 package models;
+import java.util.ArrayList;
 
+//Composição e Polimorfismo
 public class Academia {
+    private String nomeAcademia;
+    private ArrayList<Pessoa> pessoas;
+
+    public Academia(String nomeAcademia) {
+        this.nomeAcademia = nomeAcademia;
+        this.pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa){
+        pessoas.add(pessoa);
+    }
+
+    public void listarPessoas(){
+        System.out.println("Lista de Pessoas na Academia:");
+        for (Pessoa pessoa : pessoas){
+            System.out.println(pessoa.getDescricao());
+        }
+    }
 }
